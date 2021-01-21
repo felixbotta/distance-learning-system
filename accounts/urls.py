458a2c4a04cbfa9b4e1609fd8f72466e950b02d2
felixbotta <felix.botta@gmail.com>
2021-01-21 16:19:12 -0300

    Módulo de Autenticação, Cadastro do Curso e Matrícula Completos

---
+++ accounts/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
+
+from accounts import views
+
+app_name = 'accounts'
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('entrar/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('sair/', LogoutView.as_view(next_page='core:home'), name='logout'),
+    path('cadastre-se/', views.register, name='register'),
+    path('nova-senha/', views.password_reset, name='password_reset'),
+    path('confirmar-nova-senha/<key>', views.password_reset_confirm, name='password_reset_confirm'),
+    path('editar/', views.edit, name='edit'),
+    path('editar-senha/', views.edit_password, name='edit_password'),
+]
